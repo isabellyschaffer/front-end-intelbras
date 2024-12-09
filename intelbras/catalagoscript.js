0b4e0fd8c93405116a33c44e407ece618af975e2
isabellyschaffer <163335686+isabellyschaffer@users.noreply.github.com>
2024-12-09 16:48:08 -0300

    Add files via upload

---
+++ intelbras/catalagoscript.js
@@ -0,0 +1,7 @@
+function abrirModal(modalId) {
+    document.getElementById(modalId).style.display = 'flex';
+}
+
+function fecharModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
---
+++ intelbras/script.js
@@ -0,0 +1,8 @@
+
+function abrirModal(id) {
+    document.getElementById(id).style.display = "flex";
+}
+
+function fecharModal(id) {
+    document.getElementById(id).style.display = "none";
+}
